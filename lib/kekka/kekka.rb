a631e55946071d6387a8647702bf22e97024c6e2
froderik <fredrik@eldfluga.se>
2012-10-25 18:11:57 +0200

    basic parsing of boats, events and races

--- lib/kekka/kekka.rb
+++ lib/kekka/kekka.rb
@@ -1,21 +1,47 @@
 require 'nokogiri'
 
-class Kekka
+class Kekka < Hash
   def self.parse string_or_io
     document = Nokogiri.parse string_or_io
-    boat_nodes = document.css 'boat'
-    result = {}
-    boats = {}
+    result = Kekka.new
 
-    boat_nodes.each do |one_boat_node|
-      boats[one_boat_node['boatid']] = self.hash_from_node one_boat_node
-    end
-    result[:boats] = boats
+    result[:boats] = boats_from document
+    result[:events] = events_from document
     result
   end
 
+  def self.boats_from document
+    Hash[
+      document.css( 'boat' ).map do |one_boat_node|
+        [one_boat_node['boatid'], self.hash_from_node( one_boat_node )]
+      end
+    ]
+  end
+
+  def self.events_from document
+    key_value_pairs = document.css( 'event' ).map do |one_event_node|
+      attributes_hash = self.hash_from_node( one_event_node )
+      attributes_hash[:races] = self.races_from one_event_node
+      [one_event_node['eventid'], attributes_hash]
+    end
+
+    Hash[key_value_pairs]
+  end
+
+  def self.races_from event_node
+    key_value_pairs = event_node.css( 'race' ).map do |one_race_node|
+      [one_race_node['raceid'], self.hash_from_node( one_race_node )]
+    end
+    Hash[key_value_pairs]
+  end
+
+  private
+
   def self.hash_from_node one_node
     attrs = one_node.attributes
-    Hash[attrs.map {|k,v| [k, v.value]}]
+    key_value_pairs = attrs.map do |key,nokogiri_attribute|
+      [key, nokogiri_attribute.value]
+    end
+    Hash[key_value_pairs]
   end
 end
--- spec/kekka_spec.rb
+++ spec/kekka_spec.rb
@@ -14,5 +14,23 @@ describe Kekka do
         one_boat_hash.should have_keys(['boatname', 'sailnumber', 'boatid'])
       end
     end
+
+    it "should return a jolle" do
+      subject[:boats]['7']['boatname'].should == 'jolle'
+      subject[:boats]['7']['sailnumber'].should == 'FRA 739'
+    end
+
+    it "should return events" do
+      subject[:events].size.should == 1
+      subject[:events]['9'].should have_keys(['title', 'eventid'])
+    end
+
+    it "should return races" do
+      subject[:events]['9'][:races].size.should == 10
+      subject[:events]['9'][:races].each do |race_id, race_hash|
+        race_keys = ['racename', 'racenumber', 'racestartdate', 'racestarttime', 'racestatus', 'raceid']
+        race_hash.should have_keys(race_keys)
+      end
+    end
   end
 end
