5d439af6b35ba22f2796981cce57a5882baf6325
froderik <fredrik@eldfluga.se>
2012-10-25 18:28:01 +0200

    introduced more levels of describe and tested internals of a race

--- spec/kekka_spec.rb
+++ spec/kekka_spec.rb
@@ -2,35 +2,52 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Kekka do
   describe 'fleet racing' do
-    subject { Kekka.parse( open('spec/testdata/fleet-racing-example.xml')) }
+    let(:kekka) { Kekka.parse( open('spec/testdata/fleet-racing-example.xml')) }
 
-    it "should return something" do
-      subject.should_not be_nil
-    end
+    describe 'top level' do
+      subject { kekka }
 
-    it "should return boats" do
-      subject[:boats].size.should == 9
-      subject[:boats].each do |boat_id, one_boat_hash|
-        one_boat_hash.should have_keys(['boatname', 'sailnumber', 'boatid'])
+      it "should return something" do
+        subject.should_not be_nil
+      end
+
+      it "should return boats" do
+        subject[:boats].size.should == 9
+        subject[:boats].each do |boat_id, one_boat_hash|
+          one_boat_hash.should have_keys(['boatname', 'sailnumber', 'boatid'])
+        end
+      end
+
+      it "should return a jolle" do
+        subject[:boats]['7']['boatname'].should == 'jolle'
+        subject[:boats]['7']['sailnumber'].should == 'FRA 739'
+      end
+
+      it "should return events" do
+        subject[:events].size.should == 1
+        subject[:events]['9'].should have_keys(['title', 'eventid'])
       end
     end
 
-    it "should return a jolle" do
-      subject[:boats]['7']['boatname'].should == 'jolle'
-      subject[:boats]['7']['sailnumber'].should == 'FRA 739'
-    end
+    describe 'one event' do
+      subject { kekka[:events]['9'] }
 
-    it "should return events" do
-      subject[:events].size.should == 1
-      subject[:events]['9'].should have_keys(['title', 'eventid'])
-    end
+      it "should return races" do
+        subject[:races].size.should == 10
+        subject[:races].each do |race_id, race_hash|
+          race_keys = ['racename', 'racenumber', 'racestartdate', 'racestarttime', 'racestatus', 'raceid']
+          race_hash.should have_keys(race_keys)
+        end
+      end
 
-    it "should return races" do
-      subject[:events]['9'][:races].size.should == 10
-      subject[:events]['9'][:races].each do |race_id, race_hash|
-        race_keys = ['racename', 'racenumber', 'racestartdate', 'racestarttime', 'racestatus', 'raceid']
-        race_hash.should have_keys(race_keys)
+      it "should return specifics about a race" do
+        subject[:races]['383']['racename'     ].should == '6'
+        subject[:races]['383']['racenumber'   ].should == '6'
+        subject[:races]['383']['racestartdate'].should == '20100128'
+        subject[:races]['383']['racestarttime'].should == '111000'
+        subject[:races]['383']['racestatus'   ].should == 'Provisional'
       end
     end
+
   end
 end
