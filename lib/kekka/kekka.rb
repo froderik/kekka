535a414f0b294bc5fa675dff5e88f965f5645026
froderik <fredrik@eldfluga.se>
2012-10-25 18:42:24 +0200

    made the code incomprehensible.... and tiny - yay

--- lib/kekka/kekka.rb
+++ lib/kekka/kekka.rb
@@ -5,37 +5,23 @@ class Kekka < Hash
     document = Nokogiri.parse string_or_io
     result = Kekka.new
 
-    result[:boats] = boats_from document
-    result[:events] = events_from document
+    result[:boats] = hash_from document, 'boat'
+    result[:events] = hash_from document, 'event' do |attributes_hash, node|
+      attributes_hash[:races] = hash_from node, 'race'
+    end
     result
   end
 
-  def self.boats_from document
-    Hash[
-      document.css( 'boat' ).map do |one_boat_node|
-        [one_boat_node['boatid'], self.hash_from_node( one_boat_node )]
-      end
-    ]
-  end
-
-  def self.events_from document
-    key_value_pairs = document.css( 'event' ).map do |one_event_node|
-      attributes_hash = self.hash_from_node( one_event_node )
-      attributes_hash[:races] = self.races_from one_event_node
-      [one_event_node['eventid'], attributes_hash]
-    end
-
-    Hash[key_value_pairs]
-  end
-
-  def self.races_from event_node
-    key_value_pairs = event_node.css( 'race' ).map do |one_race_node|
-      [one_race_node['raceid'], self.hash_from_node( one_race_node )]
-    end
-    Hash[key_value_pairs]
-  end
-
   private
+
+  def self.hash_from node, name
+    key_value_pairs = node.css( name ).map do |matching_node|
+      attributes_hash = self.hash_from_node( matching_node )
+      yield attributes_hash, matching_node if block_given?
+      [matching_node["#{name}id"], attributes_hash]
+    end
+    Hash[key_value_pairs]
+  end
 
   def self.hash_from_node one_node
     attrs = one_node.attributes
